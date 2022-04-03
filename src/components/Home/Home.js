80df22e43ea164b97224fb987ca464fac54c303e
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-04-04 04:47:39 +0600

    home page half added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Home.css'
+import '../Assets/black chair.jpg'
+
+
 
 const Home = () => {
     return (
-        <div>
-            <h3>This is Home</h3>
+        <div className='parent-div'>
+            <div className='explore-product'>
+                <h3>This's Productive <span className='product-color'>Chair</span></h3>
+
+            </div>
+            <div className='photo-show'>
+                <img src={Image} alt="" />
+            </div>
+
         </div>
     );
 };
