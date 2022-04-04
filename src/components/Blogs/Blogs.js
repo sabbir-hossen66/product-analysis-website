b00190aad742aa1bb27bb82cdd0051ec72abafc5
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-04-05 03:23:35 +0600

    blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h3>It's a Blogs component</h3>
-        </div>
+
+        <ul>
+            <li className='api-design'>
+                Context-api : context is also touted as an easier.lighter approach to state management using redux. redux has it's disadvantages and that's why it's important to know what context api gives us the redux doesn't  React.create Context() returns a consumer and a provider.Provider is a component.Consumer as it so happens is a component that consumers and uses the state.
+            </li><br />
+            <li className='semantic-design'>
+                Semantic tag: html is a markup language.here, there are different kind of tag.semantic is one of them.such as- .*article,
+                *aside.
+                *footer.
+                *header.
+                *nav-.
+                *main. etc are the semantic tag
+            </li>
+        </ul>
+
+
     );
 };
 
