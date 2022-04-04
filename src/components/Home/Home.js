2d3484ed8ce1eddeb4ad0dd79e2d307d1d1e2b23
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-04-05 02:41:06 +0600

    home page complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import './Home.css'
 import Image from '../Assets/chair-photo.jpg'
 import UserReview from '../UserReview/UserReview';
 import Review from '../Review/Review';
+import { useNavigate, useNavigationType } from 'react-router-dom';
 
 
 
@@ -10,9 +11,10 @@ import Review from '../Review/Review';
 const Home = () => {
     const [reviews, setReviews] = UserReview([])
     const homeReviewData = reviews.slice(0, 3)
+    const navigate = useNavigate()
 
     return (
-        <div>
+        <div className='main-container'>
             <div className='flex-container'>
                 <div className='explore-product'>
                     <h2>This's Productive <span className='product-color'>Chair</span></h2>
@@ -25,10 +27,16 @@ const Home = () => {
 
             </div>
             {/* review */}
+            <h2 className='user-review'>User Review</h2>
             <div className='review-container'>
                 {
                     homeReviewData.map(review => <Review key={review.id} review={review}></Review>)
+
                 }
+
+            </div>
+            <div className='button-container'>
+                <button className='review-button' onClick={() => navigate('/reviews')}>Reviews </button>
             </div>
         </div>
     );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,15 +4,20 @@ import './Review.css'
 const Review = ({ review }) => {
     const { name, description, rating, photo, email } = review;
     return (
-        <div className='container'>
-            <div className='user-container'>
-                <img className='photo-size' src={photo} alt="" />
-                <h2>{name}</h2>
-                <p>description: {description}</p>
-                <p>email: {email}</p>
-                <p><small>rating: {rating}</small></p>
-            </div>
+
+
+        <div className='user-container'>
+
+            <img className='photo-size' src={photo} alt="" />
+
+            <h2>{name}</h2>
+            <p>description: {description}</p>
+            <p>email: {email}</p>
+            <p><small>rating: {rating}</small></p>
+
         </div>
+
+
     );
 };
 
