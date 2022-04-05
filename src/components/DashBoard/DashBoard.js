ea6049a93712db2f4e606b1fe5b427f113dbb229
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-04-05 06:02:48 +0600

    chart set up and added

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,66 @@
 import React from 'react';
-
+import { Bar, BarChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 const DashBoard = () => {
+    const data = [
+        {
+            month: "Mar",
+            investment: 100000,
+            sell: 241,
+            revenue: 10401,
+        },
+        {
+            month: "Apr",
+            investment: 200000,
+            sell: 423,
+            revenue: 24500,
+        },
+        {
+            month: "May",
+            investment: 500000,
+            sell: 726,
+            revenue: 67010,
+        },
+        {
+            month: "Jun",
+            investment: 500000,
+            sell: 529,
+            revenue: 40405,
+        },
+        {
+            month: "Jul",
+            investment: 600000,
+            sell: 601,
+            revenue: 50900,
+        },
+        {
+            month: "Aug",
+            investment: 700000,
+            sell: 670,
+            revenue: 61000,
+        },
+    ];
     return (
-        <div>
-            <h3>DashBoard its</h3>
+        <div className='parent-chart-container'>
+            <div className="my-5">
+                <LineChart width={500} height={300} data={data}>
+                    <Line dataKey={"month"}></Line>
+
+                    <Line dataKey={"sell"}></Line>
+                    <XAxis></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <div>
+                <BarChart width={500} height={300} data={data}>
+                    <Bar dataKey={"revenue"}></Bar>
+                    <Bar style={{ backgroundColor: "orange" }} dataKey={"investment"}></Bar>
+                    <XAxis></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                </BarChart>
+            </div>
         </div>
     );
 };
